b774c123cccec3caf04b690770751705a0489879
ELDADBRHANO <ebrhano@gmail.com>
2022-12-23 10:49:52 +0200

    change file name to crawl

--- src/crawl.js
+++ src/crawl.js
@@ -3,15 +3,12 @@ import cheerio from "cheerio";
 import urlParser from "url";
 import fs from "fs";
 
-const url = process.argv[2];
-const maxDepth = parseInt(process.argv[3]);
-const { host, protocol } = urlParser.parse(url);
 const seenUrls = {};
 
-async function crawl(currentPageUrl, currentDepth) {
-  if (seenUrls[currentPageUrl]) return;
-  if (currentDepth === maxDepth) return;
+async function crawl(currentPageUrl, currentDepth, maxDepth) {
+  if (seenUrls[currentPageUrl] && currentDepth === maxDepth) return;
   seenUrls[currentPageUrl] = true;
+  const { host, protocol } = urlParser.parse(currentPageUrl);
 
   let data = { results: [] };
   const sourcePage = await fetch(currentPageUrl);
@@ -33,7 +30,7 @@ async function crawl(currentPageUrl, currentDepth) {
     });
   }
 
-  const urls = filterValidUrls(currentDepthUrls);
+  const urls = filterValidUrls(currentDepthUrls,host,protocol);
   for (let i = 0; i < urls.length; i++) {
     if (currentDepth < maxDepth) {
       crawl(urls[i], currentDepth++);
@@ -41,18 +38,20 @@ async function crawl(currentPageUrl, currentDepth) {
       writeJsonToFile(data);
       return console.log("crawl end.");
     }
-    // console.log(urls[i],'42');
+    // console.log(urls[i],'-- url[i]');
   }
   // console.log(seenUrls);
 }
 
 const startCrawler = async () => {
-  await crawl(url, 0);
+  const url = process.argv[2];
+  const maxDepth = parseInt(process.argv[3]);
+  await crawl(url, 0, maxDepth);
 };
 startCrawler();
 
-const filterValidUrls = (links) => {
-  // console.log(links,'53');
+const filterValidUrls = (links,host,protocol) => {
+  // console.log(links,'links');
   return links
     .filter((link) => {
       return (
@@ -65,9 +64,9 @@ const filterValidUrls = (links) => {
       } else if (link.startsWith("?")) {
         return `${protocol}//${host}${link}`;
       } else return link;
-      // TODO filter visited links;
     });
 };
+
 
 const writeJsonToFile = (data) => {
   const isExist = fs.existsSync("./results.json");
